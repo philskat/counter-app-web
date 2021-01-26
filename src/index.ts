c436ffcccdd47a8f05f00192041759ea47377502
philskat <skatphil@gmail.com>
2021-01-26 15:58:14 +0100

    Added PWA functionality The App can now be installed as a PWA on your phone or desktop

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 import './components/Counter';
 import './index.scss';
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 const MODE = process.env.NODE_ENV || 'development';
 
@@ -12,10 +15,35 @@ module.exports = {
     filename: 'main.min.js',
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public/index.html'),
     }),
     new MiniCssExtractPlugin(),
+    new GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: '/',
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'sites',
+          },
+        },
+      ],
+      skipWaiting: true,
+      clientsClaim: true,
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public/manifest.webmanifest'),
+        },
+        {
+          from: path.resolve(__dirname, 'img'),
+          to: 'img',
+        },
+      ],
+    }),
   ],
   resolve: {
     extensions: ['.ts', '.js'],
